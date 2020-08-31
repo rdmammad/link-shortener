687bca19071473c74486b0766b1caa9545dee173
Raida Mammadova <raida.mammadova>
2020-08-31 14:56:18 +0400

    user authentication done

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,8 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import {useHttp} from "../hooks/http.hook";
 import {useMessage} from "../hooks/message.hook";
+import {AuthContext} from "../context/auth.context";
 
 const Auth = () => {
+    const auth = useContext(AuthContext)
     const message = useMessage()
     const {loading, request, error, clearError} = useHttp()
     const [form, setForm] = useState({
@@ -34,7 +36,7 @@ const Auth = () => {
     const loginHandler = async () => {
         try {
             const data = await request('/api/auth/login', 'POST', {...form})
-            message(data.message)
+            auth.login(data.token, data.userId)
         } catch (e) {
 
         }
