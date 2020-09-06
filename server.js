f033a1110c87cf469b34d7ee32691b2ef18d2c69
Raida Mammadova <raida.mammadova>
2020-09-06 17:50:33 +0400

    some scripting changes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const config = require("config");
-const mongoose = require("mongoose");
+const express = require('express')
+const config = require("config")
+const path = require('path')
+const mongoose = require("mongoose")
 
-const app = express();
+const app = express()
 
 // body parser middleware that by default in express
 app.use(express.json({extended: true }))
@@ -11,7 +12,15 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/link', require('./routes/link.routes'))
 app.use('/t', require('./routes/redirect.routes'))
 
-const PORT = config.get('port') || 5000;
+if (process.env.NODE_ENV === 'production'){
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const PORT = config.get('port') || 5000
 
 async function start(){
     try {
@@ -20,11 +29,11 @@ async function start(){
             useUnifiedTopology: true,
             useCreateIndex: true,
         })
-        app.listen(PORT, ()=> console.log(`App has been started on port ${PORT}...`));
+        app.listen(PORT, ()=> console.log(`App has been started on port ${PORT}...`))
     }catch (e) {
-        console.log("Server error:", e.message);
-        process.exit(1);
+        console.log("Server error:", e.message)
+        process.exit(1)
     }
 }
 
-start();
+start()
